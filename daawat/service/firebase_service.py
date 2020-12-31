343a8a28dcd063e6d8c5cb892229dff9326dee13
SAAD <57146004+SaadFurqhan@users.noreply.github.com>
2020-12-31 17:44:24 +0530

    Update firebase_service.py

--- daawat/service/firebase_service.py
+++ daawat/service/firebase_service.py
@@ -4,7 +4,7 @@ config = {
     "apiKey": "AIzaSyCi5-vfmk7VW2xFtgacj3_arjL04KeaW2A",
     "authDomain": "dawaat-9e0f1.firebaseapp.com",
     "projectId": "dawaat-9e0f1",
-    "databaseURL":"https://dawaat-9e0f1-default-rtdb.firebaseio.com",
+#     "databaseURL":"https://dawaat-9e0f1-default-rtdb.firebaseio.com",
     "storageBucket": "dawaat-9e0f1.appspot.com",
     "messagingSenderId": "55134356040",
     "appId": "1:55134356040:web:ea4f51040695d9d59f0e7d",
