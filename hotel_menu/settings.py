d7174cae1d21045d8c0c164f289660724d3b5af6
SAAD <57146004+SaadFurqhan@users.noreply.github.com>
2020-12-31 15:55:24 +0530

    Update settings.py

--- hotel_menu/settings.py
+++ hotel_menu/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '1to#em8#9%nfk)vg)c9@6q2-bjvhbw+liu@ksao!kroggn7igx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['dukaan-ecommerce-website.herokuapp.com','121.0.0.1']
+ALLOWED_HOSTS = ['dawaat.herokuapp.com/','121.0.0.1']
 
 
 # Application definition
