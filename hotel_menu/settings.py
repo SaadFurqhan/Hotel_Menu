1dacf4e75f49a3dfb62948a68229aa142430b0ae
SAAD <57146004+SaadFurqhan@users.noreply.github.com>
2021-01-12 13:35:23 +0530

    Update settings.py

--- hotel_menu/settings.py
+++ hotel_menu/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '1to#em8#9%nfk)vg)c9@6q2-bjvhbw+liu@ksao!kroggn7igx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['dawaat.herokuapp.com/','121.0.0.1']
 
